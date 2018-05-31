4b200bc974d54e69672d4b0d02aa6aa9e2dbb60b
silverdragonia <silverdragonia@gmail.com>
2018-05-31 15:54:54 -0400

    deleting useless table rb

--- db/migrate/20180531054332_create_entries_table.rb
+++
@@ -1,8 +0,0 @@
-class CreateEntriesTable < ActiveRecord::Migration[5.2]
-  def change
-    create_table :entries do |entry|
-      entry.integer :user
-      entry.integer :post
-    end
-  end
-end
